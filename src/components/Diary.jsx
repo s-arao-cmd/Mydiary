9317ee764e063726c5d99d29032f59683713fb91
Arao <ara@example.com>
2026-06-22 14:18:23 +0900

    Diary.jsxの追加

---
+++ src/components/Diary.jsx
@@ -0,0 +1,5 @@
+export default function Diary() {
+  return (
+    <div>Diary</div>
+  )
+}
---
+++ src/components/DiaryList.jsx
@@ -0,0 +1,7 @@
+export default function DiaryList() {
+  return (
+    <div>
+        {/* Diaryコンポーネントをループ*/}
+    </div>
+  )
+}
